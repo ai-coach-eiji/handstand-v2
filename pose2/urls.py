5e45a686d57f801fa307868d86f55f5ab2734f97
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-03-30 13:09:55 +0900

    poseパスの削除

--- pose2/urls.py
+++ pose2/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pose/', include('pose_estimation.urls')),
+    path('', include('pose_estimation.urls')),
 ]
